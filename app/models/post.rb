b3e12ac16937d2a146199f79b112341ec06e7d9d
lwhelmut <wilkeleo.1021@gmail.com>
2021-03-26 11:31:52 +0900

    comment change

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   has_rich_text :concept
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   acts_as_taggable
   
